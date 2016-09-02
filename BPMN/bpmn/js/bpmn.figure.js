171d9f7908235a578357761671d03ea03a33f605
microbit <microbit@163.com>
2016-09-02 18:44:01 +0800

    提交修改

---
+++ BPMN/bpmn/js/bpmn.figure.js
@@ -0,0 +1,109 @@
+﻿/******************************图形模版库*******************************/
+
+//矩形
+var Rectangle = function (options) {
+    var defaultOpt = {
+        text: "任务",
+        category: "rectangle",
+        type: "task",
+        x: 0,
+        y: 5,
+        width: 40,
+        height: 40,
+        fillWidth: 40,
+        fillHeight: 30,
+        bgcolor: "#3EB3E4"
+    };
+    var obj = new Object;
+    obj.options = $.extend(defaultOpt, options);
+    obj.getCanvas = Rectangle.prototype.getCanvas;
+    return obj;
+}
+Rectangle.prototype.getCanvas = function () {
+    var opt = this.options;
+    var c = document.createElement("Canvas");
+    c.setAttribute("width", opt.width);
+    c.setAttribute("height", opt.height);
+    var ctx = c.getContext("2d");
+    ctx.fillStyle = opt.bgcolor;
+    ctx.fillRect(opt.x, opt.y, opt.fillWidth, opt.fillHeight);
+    return c;
+}
+
+//圆形
+var Circle = function (options) {
+    var defaultOpt = {
+        text: "开始",
+        category: "circle",
+        type: "startEvent",
+        x: 20,
+        y: 20,
+        width: 40,
+        height: 40,
+        radius: 19,
+        bgcolor: "#91F7CE"
+    };
+    var obj = new Object;
+    obj.options = $.extend(defaultOpt, options);
+    obj.getCanvas = Circle.prototype.getCanvas;
+    return obj;
+}
+Circle.prototype.getCanvas = function () {
+    var opt = this.options;
+    var c = document.createElement("Canvas");
+    c.setAttribute("width", opt.width);
+    c.setAttribute("height", opt.height);
+    var ctx = c.getContext("2d");
+    ctx.beginPath();
+    ctx.arc(opt.x, opt.y, opt.radius, 0, 2 * Math.PI);
+    ctx.fillStyle = opt.bgcolor;
+    ctx.closePath();
+    ctx.fill();
+    return c;
+}
+
+//菱形
+var Diamond = function (options) {
+    var defaultOpt = {
+        text: "网关",
+        category: "diamond",
+        type: "gateway",
+        x: 20,
+        y: 20,
+        width: 40,
+        height: 40,
+        radius: 20,
+        bgcolor: "#FFAA25"
+    };
+    var obj = new Object;
+    obj.options = $.extend(defaultOpt, options);
+    obj.getCanvas = Diamond.prototype.getCanvas;
+    return obj;
+}
+Diamond.prototype.getCanvas = function () {
+    var opt = this.options;
+    var c = document.createElement("Canvas");
+    c.setAttribute("width", opt.width);
+    c.setAttribute("height", opt.height);
+    var ctx = c.getContext("2d");
+    ctx = drawPath(ctx, opt.x, opt.y, 4, opt.radius)
+    ctx.fillStyle = opt.bgcolor;
+    ctx.fill();
+    return c;
+}
+
+//画多边形方法
+function drawPath(context, x, y, n, r) {
+    var i, ang;
+    ang = Math.PI * 2 / n //旋转的角度
+    context.lineWidth = 1;//设置线宽
+    context.translate(x, y);//原点移到x,y处，即要画的多边形中心
+    context.moveTo(0, -r);//据中心r距离处画点
+    context.beginPath();
+    for (i = 0; i < n; i++) {
+        context.rotate(ang)//旋转
+        context.lineTo(0, -r);//据中心r距离处连线
+    }
+    context.closePath();
+    return context;
+}
---
+++ BPMN/bpmn/js/bpmn.figure.panel.js
@@ -0,0 +1,47 @@
+﻿/******************************初始化左边形状面板和右边按钮工具栏*******************************/
+
+$(function () {
+    //var $bpmn_wrapper = $(".bpmn_wrapper:first");
+    //var $figure_panel = $(".figure_panel:first");//形状面板
+    //var $drawing_wrapper = $(".drawing_wrapper:first");
+    ////右侧画布区宽度自适应
+    //var bpmn_wrapper_width = $bpmn_wrapper.innerWidth();
+    //var figure_panel_width = $figure_panel.outerWidth();
+    //var drawing_wrapper_width = bpmn_wrapper_width - figure_panel_width;
+    //$drawing_wrapper.css("width", drawing_wrapper_width);
+
+    //初始化形状面板，只需要加载一次
+    InitFigurePanel();
+})
+
+//初始化形状面板
+function InitFigurePanel() {
+    var figureArray = new Array();//存储形状实例
+    figureArray.push(new Rectangle());//推入任务模型
+    figureArray.push(new Circle());//推入开始事件模型
+    figureArray.push(new Diamond());//推入网关模型
+    figureArray.push(new Circle({ text: "结束", type: "endEvent", bgcolor: "#FF8F80" }));//推入结束事件模型
+    var figure_panel = document.getElementsByClassName("figure_panel")[0];//形状面板
+
+    //装载形状模型到面板中，canvas绘制
+    for (var item in figureArray) {
+        var o = document.createElement("div");
+        o.setAttribute("class", "figure_box");
+        o.setAttribute("name", figureArray[item].options.type);
+        var s = document.createElement("span");
+        s.innerText = figureArray[item].options.text;
+        o.appendChild(figureArray[item].getCanvas());
+        o.appendChild(s);
+        figure_panel.appendChild(o);
+    }
+
+    //绑定鼠标事件
+    $(".figure_box").mouseover(function () {
+        $(this).removeClass("figure_box_over").addClass("figure_box_over");
+    }).mouseout(function () {
+        $(this).removeClass("figure_box_over");
+    }).mousedown(function () {
+        $(".figure_box").removeClass("figure_box_down")
+        $(this).addClass("figure_box_down");
+    });
+}
